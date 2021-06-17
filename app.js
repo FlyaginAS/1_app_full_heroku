9de021bd029b6765f2e5f0cf9a17f8db8ad59a05
FlyaginAs <rus.reg.36@gmail.com>
2021-06-17 15:01:03 +0300

    change path

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path=require('path');
 const cors = require('cors');
 const morgan = require('morgan');
 const articleRouter = require('./routes/articleRoutes');
@@ -10,7 +11,8 @@ const sourceRouter = require('./routes/sourceRoutes');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(morgan('dev'));
 // app.use((req, res, next) => {
 //   console.log('this is my middleware');
